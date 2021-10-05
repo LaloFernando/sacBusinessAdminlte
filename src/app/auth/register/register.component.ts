4057e8199abb4427bd557e9766ef29edd8c1439e
lalofernando <newsystemsac2010@gmail.com>
2021-10-04 19:34:37 -0500

    validaciones del register

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -16,13 +16,15 @@ export class RegisterComponent {
 
   public registerForm = this.fb.group({
     US_CODCIA: ['1'],
-    US_CODIGO: ['ERICK',[Validators.required]],
+    US_CODIGO: ['',[Validators.required]],
     PE_CODIGO: ['00000002'],
-    US_PASSWORD: ['123',[Validators.required]],
-    password2: ['123',[Validators.required]],
+    US_PASSWORD: ['',[Validators.required]],
+    password2: ['',[Validators.required]],
     US_PERMISO: ['',[Validators.required]],
     US_CODBODE: ['01'],
     US_SERIEVTA: ['001-002']
+  },{
+    validators: this.passwordsIguales('US_PASSWORD','password2')
   });
 
   constructor(private fb:FormBuilder, private router:Router, private usuarioSvc:UsuarioService) { }
@@ -55,4 +57,39 @@ export class RegisterComponent {
       onlySelf:true
     })
   }
+
+  campoNoValido(campo:string):boolean{
+    if ( this.registerForm.get(campo)?.invalid && this.formSubmitted ) {
+
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  contrasenaNoValidas(){
+
+    const pass1 = this.registerForm.get('US_PASSWORD')?.value;
+    const pass2 = this.registerForm.get('password2')?.value;
+
+    if ((pass1 !== pass2) && this.formSubmitted) {
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  passwordsIguales(pass1Name: string, pass2Name: string){
+    return (formGroup: FormGroup) => {
+      const pass1Control = formGroup.get(pass1Name);
+      const pass2Control = formGroup.get(pass2Name);
+
+      if (pass1Control?.value === pass2Control?.value) {
+        pass2Control?.setErrors(null);
+      }else{
+        pass2Control?.setErrors({noEsIgual:true});
+      }
+    }
+  }
+
 }
